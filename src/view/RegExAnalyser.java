fa94cce62e64b7e43901269928dad4fac1de2890
Makhles <mr.lange85@gmail.com>
2017-06-19 22:28:48 -0300

    Added document. Epsilon is now present at the allowed vocabulary symbols.

--- src/view/RegExAnalyser.java
+++ src/view/RegExAnalyser.java
@@ -82,7 +82,7 @@ public class RegExAnalyser extends JFrame {
 
         vocabularySymbols.addAll(
                 Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                        's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'));
+                        's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '&'));
 
         stateSymbols.addAll(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
                 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'));
